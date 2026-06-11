8a79ba44b6141e1286be9ba2c985451d95e33ea4
Khauan <khauan@acad.ifma.edu.br>
2026-06-10 21:35:36 -0300

    -Validações de cadastro de usuario-

--- src/main/java/com/minigoodreads/api/repositories/UsuarioRepository.java
+++ src/main/java/com/minigoodreads/api/repositories/UsuarioRepository.java
@@ -3,7 +3,10 @@ package com.minigoodreads.api.repositories;
 import com.minigoodreads.api.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByNick(String nick);
 }
--- src/main/java/com/minigoodreads/api/service/UsuarioService.java
+++ src/main/java/com/minigoodreads/api/service/UsuarioService.java
@@ -1,11 +1,9 @@
 package com.minigoodreads.api.service;
-
 import com.minigoodreads.api.DTO.request.DadosAtualizacaoUsuario;
 import com.minigoodreads.api.DTO.request.DadosNovoUsuario;
 import com.minigoodreads.api.DTO.response.DadosUsuario;
 import com.minigoodreads.api.models.Usuario;
 import com.minigoodreads.api.repositories.UsuarioRepository;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +13,54 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
-    @Autowired private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-    public DadosUsuario registrarUsuario(DadosNovoUsuario dados){
+    public DadosUsuario registrarUsuario(DadosNovoUsuario dados) {
+        String retorno = verifica(dados);
+        if(!retorno.isBlank()){
+            throw new RuntimeException(retorno);
+        }
         var novoUsuario = new Usuario(dados);
         usuarioRepository.save(novoUsuario);
         return new DadosUsuario(novoUsuario);
     }
 
-    public DadosUsuario detalharUsuario(Long id){
+    public DadosUsuario detalharUsuario(Long id) {
         var dadosUsuario = usuarioRepository.findById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Digita certo aí"));
+                .orElseThrow(() -> new EntityNotFoundException("Digita certo aí"));
         return new DadosUsuario(dadosUsuario);
     }
+
     @Transactional
-    public DadosUsuario atualizar(Long id, DadosAtualizacaoUsuario dados){
+    public DadosUsuario atualizar(Long id, DadosAtualizacaoUsuario dados) {
         var usuario = usuarioRepository.findById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Digita certo ai"));
+                .orElseThrow(() -> new EntityNotFoundException("Digita certo ai"));
         usuario.atualizar(dados);
         return new DadosUsuario(usuario);
     }
-    public ResponseEntity deletar(Long id){
+
+    public ResponseEntity<?> deletar(Long id) {
         usuarioRepository.findById(id)
-                .orElseThrow(()-> new EntityNotFoundException("Não achou..."));
+                .orElseThrow(() -> new EntityNotFoundException("Não achou..."));
         usuarioRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+
+    private String verifica(DadosNovoUsuario dados) {
+        String retorno = "";
+
+        if (usuarioRepository.findByEmail(dados.email()).isPresent()) {
+            retorno += "Email em uso ";
+        }
+        if (usuarioRepository.findByNick(dados.nick()).isPresent()) {
+            retorno += "Nick em uso ";
+        }
+        if (dados.senha().length() < 6) {
+            retorno += "A senha deve conter no minimo 6 caracteres ";
+        }
+        return retorno;
+    }
+
 }
